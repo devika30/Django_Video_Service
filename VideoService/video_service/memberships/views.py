a086e0a78072c209069e0b26691abf880d131c10
devika30 <olkardevika69@gmail.com>
2020-07-17 13:57:42 +0530

    updated membershsips

--- VideoService/video_service/memberships/views.py
+++ VideoService/video_service/memberships/views.py
@@ -1,7 +1,51 @@
+from django.contrib import messages
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.views.generic import ListView
-from .models import Membership
-
+from .models import Membership,UserMembership,Subscription
+from django.urls import reverse
 # Create your views here.
+
+def get_user_membership(request):
+    user_membership_qs=UserMembership.objects.filter(user=request.user)
+    print(user_membership_qs)
+    if user_membership_qs.exists():
+        return user_membership_qs.first()
+    return None
+
+def get_user_subscription(request):
+    user_subscription_qs=Subscription.objects.filter(user_membership=get_user_membership(request))
+    if user_subscription_qs.exists():
+        user_subscription=user_subscription_qs.first()
+        return user_subscription
+    return None
+
+
 class MembershipSelectView(ListView):
     model=Membership
+
+    def get_context_data(self,*args,**kwargs):
+        context=super().get_context_data(**kwargs)
+        current_membership=get_user_membership(self.request)
+        context['current_membership']=str(current_membership.membership)
+        return context
+
+    def post(self,request,**kwargs):
+        selected_membership_type=request.POST.get('membership_type')
+
+        user_membership=get_user_membership(self.request)
+        user_subscription=get_user_subscription(self.request)
+
+        selected_membership_qs=Membership.objects.filter(membership_type=selected_membership_type)
+
+        if selected_membership_qs.exists():
+            selected_membership=selected_membership_qs.first()
+
+        if user_membership.membership==selected_membership:
+            if user_subscription!=None:
+                messages.info(request,"You already have this membership.Your next payment is due{}.format('get this value from stripe')")
+                return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+
+        request.session['selected_membership_type']=selected_membership.membership_type
+
+        return HttpResponseRedirect(reverse('memberships:payment'))
--- VideoService/video_service/video_service/urls.py
+++ VideoService/video_service/video_service/urls.py
@@ -6,7 +6,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('courses.urls',namespace='courses')),
+    path('courses/',include('courses.urls',namespace='courses')),
     path('memberships/',include('memberships.urls',namespace='memberships'))
 
 ]
